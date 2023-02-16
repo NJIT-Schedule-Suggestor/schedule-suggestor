53272aa15d178243bfa175e68e451bd3fa83a939
Jumaiza <zaid.abujumaiza@gmail.com>
2023-02-16 01:17:30 -0500

    only got 16 csv files. need to scroll down with selenium to get the rest

--- newcode/coursesParser.py
+++ newcode/coursesParser.py
@@ -7,26 +7,22 @@ driver = webdriver.Chrome()
 
 driver.get("https://generalssb-prod.ec.njit.edu/BannerExtensibility/customPage/page/stuRegCrseSched")
 
-time.sleep(5)
+time.sleep(3)
 
-for i in range(97):
+button = driver.find_element(By.ID,"pbid-courseListSectionExportToExcel")
+button.click()
+time.sleep(2)
 
-    if i!=0:
-        
-        spanID = "//span[@id='pbid-subjListTableSubjectLink-"+str(i)+"']/a"
-        aTag = driver.find_element(By.XPATH,spanID)
-        aTag.click()
-        time.sleep(2)
+for i in range(1,97):
+
+    spanID = "//span[@id='pbid-subjListTableSubjectLink-"+str(i)+"']/a"
+    aTag = driver.find_element(By.XPATH,spanID)
+    aTag.click()
+    time.sleep(2)
 
     button = driver.find_element(By.ID,"pbid-courseListSectionExportToExcel")
     button.click()
-
     time.sleep(2)
 
-    download_path = "/home/cs288/Downloads"
-    filename = max([download_path + "/" + f for f in os.listdir(download_path)], key=os.path.getctime)
-
-    import shutil
-    shutil.move(filename, "/home/cs288/Desktop/schedule-suggestor-project/schedule-suggestor/newcode")
 
 driver.quit()
---
+++ newcode/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 coursesParser.py
+
+mv /home/cs288/Downloads/*.csv csv-files/
