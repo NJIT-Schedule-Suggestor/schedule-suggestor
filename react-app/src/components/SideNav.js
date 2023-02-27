82af871bd09daa1f0141f2cf72bbeb2b5f5b40b5
Jumaiza <zaid.abujumaiza@gmail.com>
2023-02-26 22:26:21 -0500

    sidenav

---
+++ react-app/src/components/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideNav.css';
+
+export function SideNav() {
+  return (
+    <div className="side-nav">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  );
+}
