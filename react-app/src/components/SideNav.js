14916387bc6b8664dc72310143715d17e4f2d303
Jumaiza <zaid.abujumaiza@gmail.com>
2023-02-26 23:30:45 -0500

    top bar and side nav visual only

--- react-app/src/components/SideNav.js
+++ react-app/src/components/SideNav.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './SideNav.css';
+import njitImage from "./Logo_of_New_Jersey_Institute_of_Technology.png";
+import MuiButton from '@mui/material/Button';
+
 
 export function SideNav() {
   return (
     <div className="side-nav">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
+      <img src={njitImage} className='njit-image'/>
+      <a href="#">Home</a>
+      <a href="#">Courses</a>
+      <a href="#">Time Preference</a>
+      <div className='generate-button'>
+        <MuiButton variant="contained">Generate</MuiButton>
+      </div>
     </div>
   );
 }
