fe20f8325212e2394531ecdcc055d7415ece1e46
Jumaiza <zaid.abujumaiza@gmail.com>
2023-10-03 22:30:43 -0400

    fixed box container padding and nav bar text and stuff. use vw and vh from now on

--- react-app/src/components/BoxContainer.js
+++ react-app/src/components/BoxContainer.js
@@ -2,15 +2,8 @@ import React from 'react';
 import './BoxContainer.css';
 
 export function BoxContainer() {
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     return (
-        <div className="shift-right">
-            <div className="box-container">
-
-            </div>
+        <div className="box-container">
         </div>
     );
 }
-
-
-
