0fff0da21cdf22aa701c496ebc4a304d809ba484
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2023-10-03 21:40:46 -0400

    box container

--- react-app/src/components/BoxContainer.js
+++ react-app/src/components/BoxContainer.js
@@ -4,7 +4,11 @@ import './BoxContainer.css';
 export function BoxContainer() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     return (
-        <div className="box-container"> </div>
+        <div className="shift-right">
+            <div className="box-container">
+
+            </div>
+        </div>
     );
 }
 
