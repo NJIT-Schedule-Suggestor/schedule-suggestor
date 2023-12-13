8797d8d9c42ec83131cf5f5f584649f8d542fe49
Jumaiza <zaid.abujumaiza@gmail.com>
2023-12-13 02:33:43 -0500

    use props buddy

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -4,18 +4,16 @@ import './Header.css';
 
 // This is for the header. Each page contains the name of the page at the top left
 
-export function Header({text, size}) {
+export function Header(props) {
     //ternary operator for sizes small, medium, large
-    const headerSize = size === 'small' ? 'small-header' : size === 'medium' ? 'medium-header' : 'large-header';
-
     return (
-        <div className={`header ${headerSize}`}>
-            <h1> {text} </h1>
+        <div className={`header ${props.size}-header`}>
+            <h1> {props.text} </h1>
         </div>
     );
 }
 
 Header.propTypes = {
     text: PropTypes.string.isRequired,
-    size: PropTypes.oneOf.isRequired,
+    size: PropTypes.oneOf(['small', 'medium', 'large']).isRequired,
 }
